54eaad95bd1fd642c0b3ed104b047ab9af2d6b47
MikeNichols1371 <mnich13711@gmail.com>
2022-07-20 10:48:23 -0400

    Finished Exercise

---
+++ FactoryPattern1/Program.cs
@@ -0,0 +1,9 @@
+﻿using FactoryPattern1;
+
+Console.WriteLine("How many tires does your Vehicle have?");
+Console.WriteLine();
+var wheelNum = int.Parse(Console.ReadLine().ToLower());
+Console.WriteLine();
+IVehicle vehicle = VehicleFactory.GetVehicle(wheelNum);
+vehicle.Drive();
+
---
+++ FactoryPattern1/VehicleFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern1
+{
+    internal static class VehicleFactory
+    {
+        public static IVehicle GetVehicle(int wheelNum)
+        {
+           switch (wheelNum)
+            {
+                case 2:
+                    return new Motorcycle();
+                    break;
+                case 4:
+                    return new Car();
+                    break;
+                default:
+                    return new Car();
+                    break;
+            }
+
+                    
+           
+
+        }
+    }
+}
